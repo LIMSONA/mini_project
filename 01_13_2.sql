8ef4143aa24179abcddafe93d3e212865166d7b6
임소나 <lightsn52@gmail.com>
2022-01-13 10:00:15 +0000

    sql_01_13_02

---
+++ 01_13_2.sql
@@ -0,0 +1,12 @@
+use bladb;
+
+create table bankaccountlist(
+계좌번호 varchar(30) not null,  
+이름 varchar(20) not null,
+잔액 bigint not null);
+
+create table history(
+계좌번호 varchar(30) not null,  
+이름 varchar(20) not null,
+입출금 varchar(20) not null,
+금액 bigint not null);
